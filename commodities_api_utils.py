89291abf5224915ad0fc3e6cdc1936fc1db8aedf
Robert Johnson <robert.johnson@cabinetoffice.gov.uk>
2022-04-05 16:53:39 +0100

    tidy

--- commodities_api_utils.py
+++ commodities_api_utils.py
@@ -1,8 +1,8 @@
-
-import os
-import requests
 import json
+import os
+
 import pandas as pd
+import requests
 
 COMMODITIES_ACCESS_KEY = os.getenv("COMMODITIES_ACCESS_KEY")
 
@@ -18,14 +18,14 @@ def get_commodity_data(parameter, start_date, end_date):
     output:
     data from api in json form
     """
-        base_url = "https://commodities-api.com/api/timeseries?"
-        access_key = f"access_key={COMMODITIES_ACCESS_KEY}"
-        date = f"&start_date={start_date}&end_date={end_date}"
-        base_parameter = f"&base=USD&symbols={parameter}"
-        endpoint = f"{base_url}{access_key}{date}{base_parameter}"
-        response = requests.get(endpoint)
-        data_json = json.loads(response.text)
-        return data_json
+    base_url = "https://commodities-api.com/api/timeseries?"
+    access_key = f"access_key={COMMODITIES_ACCESS_KEY}"
+    date = f"&start_date={start_date}&end_date={end_date}"
+    base_parameter = f"&base=USD&symbols={parameter}"
+    endpoint = f"{base_url}{access_key}{date}{base_parameter}"
+    response = requests.get(endpoint)
+    data_json = json.loads(response.text)
+    return data_json
 
 
 def read_and_write_json_from_API(parameter, start_date, end_date):
@@ -51,7 +51,7 @@ def process_saved_api_data(parameter):
     """
     turn API data into useable pandas dataframe
     inputs:
-    parameter relating to saved json 
+    parameter relating to saved json
     -----
     output:
     nice pandas df
@@ -60,19 +60,12 @@ def process_saved_api_data(parameter):
     f = open(file_name)
     data = json.load(f)
     dates = []
-    for i in data['data']['rates']:
+    for i in data["data"]["rates"]:
         dates.append(i)
     commodity_values = []
     for j in dates:
-        day_dict = data['data']['rates'][j]
+        day_dict = data["data"]["rates"][j]
         value = day_dict.get(parameter)
         commodity_values.append(value)
-    df = pd.DataFrame({"date":dates, "commodity values": commodity_values})
+    df = pd.DataFrame({"date": dates, "commodity values": commodity_values})
     return df
-
-
-
- 
-
-
-
