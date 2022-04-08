66b5042bb78a88ce36eab723ac3279083fc200fb
Robert Johnson <robert.johnson@cabinetoffice.gov.uk>
2022-04-08 14:09:09 +0100

    update data

--- create_data.py
+++ create_data.py
@@ -5,7 +5,7 @@ COMMODITIES = ["WHEAT", "CORN", "BRENTOIL", "NG"]
 CURRENCIES = ["RUB", "UAH", "EUR"]
 
 START_DATE = "2022-01-05"
-END_DATE = "2022-04-03"
+END_DATE = "2022-04-06"
 
 for commodity in COMMODITIES:
     read_and_write_json_from_API(commodity, START_DATE, END_DATE)
